e300ac0456410797e0a6a0e408ba5b2a9069b8d7
Daniel <daniel@adsp.ai>
2020-12-01 21:51:28 +0000

    added ram parser

--- src/parsers/backmarket.py
+++ src/parsers/backmarket.py
@@ -4,25 +4,33 @@ from typing import Tuple
 from bs4 import BeautifulSoup
 from .base import BaseParser
 
+
 class BackmarketLaptopParser(BaseParser):
-    
     def _get_items(self):
         products = self.soup.findAll("a", {"data-qa": "productThumb"})
         return products
 
     @staticmethod
     def _parse_brand_model(product: BeautifulSoup) -> Tuple[str, str]:
-        title = product.find('h2',{'data-test':'title'}).text
+        title = product.find("h2", {"data-test": "title"}).text
         title = title.strip()
-        brand_model = re.search(r'^(.+)?\s\d{2}[.]?\d{0,2}”', title).group(1)
-        brand_model = brand_model.split(' ', 1)
+        brand_model = re.search(r"^(.+)?\s\d{2}[.]?\d{0,2}”", title).group(1)
+        brand_model = brand_model.split(" ", 1)
         return brand_model[0], brand_model[1]
 
     @staticmethod
     def _parse_processor(product: BeautifulSoup) -> str:
-        processor = product.find('ul').findAll('li')[1].find('b').text
+        processor = product.find("ul").findAll("li")[1].find("b").text
         processor = processor.strip()
         return processor
+
+    @staticmethod
+    def _parse_ram(product: BeautifulSoup) -> int:
+        ram = product.find("ul").findAll("li")[4].find("b").text
+        ram = ram.strip()
+        ram = ram.replace("GB", "")
+        ram = int(ram)
+        return ram
 
     def parse(self):
         pass
--- tests/test_parser_backmarket.py
+++ tests/test_parser_backmarket.py
@@ -16,22 +16,40 @@ def test_laptop_get_items(html_code):
     items = bls._get_items()
     assert len(items) == 30
 
-@pytest.mark.parametrize("test_input,expected", 
-                    [(0, ('Lenovo', 'ThinkPad X240')),
-                     (4, ('Dell', 'Latitude E7270')),
-                     (6, ('Dell', 'G5 15 5590'))])
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (0, ("Lenovo", "ThinkPad X240")),
+        (4, ("Dell", "Latitude E7270")),
+        (6, ("Dell", "G5 15 5590")),
+    ],
+)
 def test_laptop_parse_brand_model(html_code, test_input, expected):
     bls = BackmarketLaptopParser(html_code)
     items = bls._get_items()
     item = items[test_input]
     assert bls._parse_brand_model(item) == expected
 
-@pytest.mark.parametrize("test_input,expected", 
-                    [(0, 'Intel Core i5 1.9 GHz'),
-                     (4, 'Intel Core i5 2.4 GHz'),
-                     (6, 'Intel Core i7 1.8 GHz')])
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (0, "Intel Core i5 1.9 GHz"),
+        (4, "Intel Core i5 2.4 GHz"),
+        (6, "Intel Core i7 1.8 GHz"),
+    ],
+)
 def test_laptop_parse_processor(html_code, test_input, expected):
     bls = BackmarketLaptopParser(html_code)
     items = bls._get_items()
     item = items[test_input]
     assert bls._parse_processor(item) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [(0, 4), (4, 8), (6, 8)])
+def test_laptop_parse_ram(html_code, test_input, expected):
+    bls = BackmarketLaptopParser(html_code)
+    items = bls._get_items()
+    item = items[test_input]
+    assert bls._parse_ram(item) == expected
