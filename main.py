c560e701a2c65283f7dc4dc6f51a6dbfd99d9278
Daniel <daniel@adsp.ai>
2020-12-05 19:19:41 +0000

    Update main script with backmarket laptop scraper

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import typer
-from src.downloader import downloadHTML
+import json
+from src.parsers.backmarket import BackmarketLaptopParser
 
 
-def main(url: str):
-    html = downloadHTML(url)
-    print(html)
+def main(site: str):
+    if site == 'backmarket':
+        blp = BackmarketLaptopParser()
+        laptops = blp.parse()
+        json_file = [x.asdict() for x in laptops]
+        with open("file.json", "w") as f:
+            json.dump(json_file, f)
+    else:
+        print(f"Site {site} not implemented")
 
 
 if __name__ == "__main__":
