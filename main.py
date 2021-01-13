ae8b24de24459932883c8c4981b18f768f813433
Daniel Sharp <dsharpc@gmail.com>
2021-01-13 22:13:20 +0000

    Added tablet parser to main script

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import typer
 import json
 from typing import Optional, List
-from src.parsers.backmarket import BackmarketLaptopParser
+from datetime import datetime
+from src.parsers.backmarket import BackmarketLaptopParser, BackmarketTabletParser
 from src.summariser import Summary
 
 
@@ -9,13 +10,22 @@ app = typer.Typer()
 
 
 @app.command()
-def scrape(site: str):
+def scrape(site: str, product: str):
     if site == "backmarket":
-        blp = BackmarketLaptopParser()
-        laptops = blp.parse()
-        json_file = [x.asdict() for x in laptops]
+        if product == 'laptop':
+            blp = BackmarketLaptopParser()
+            items = blp.parse()
+        elif product == 'tablet':
+            btp = BackmarketTabletParser()
+            items = btp.parse()
+        else:
+            print(f"Product {product} not implemented for {site}")
+            raise Exception
+
+        json_file = [x.asdict() for x in items]
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         # TODO Add a function to write these json files
-        with open("file.json", "w") as f:
+        with open(f"{site}_{product}_{timestamp}.json", "w") as f:
             json.dump(json_file, f)
     else:
         print(f"Site {site} not implemented")
