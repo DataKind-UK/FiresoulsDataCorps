5f25ef15bcac90cedeae4204d501213300dc1bd6
Daniel Sharp <dsharpc@gmail.com>
2021-01-26 07:55:48 +0000

    Finished parser steps

--- src/parsers/broadbandchoices.py
+++ src/parsers/broadbandchoices.py
@@ -1,7 +1,7 @@
 """
 Class to extract broadband dongle contracts from the Broadbandchoices price comparison site
 """
-from typing import Tuple
+from typing import Tuple, List
 import re
 from bs4 import BeautifulSoup
 from .base import BaseParser
@@ -21,5 +21,40 @@ class BroadbandchoicesDongleParser(BaseParser):
         service = header.replace(provider, "", 1).strip()
         return provider, service
 
+    @staticmethod
+    def _get_data(product: BeautifulSoup) -> List[BeautifulSoup]:
+        data = product.findAll('div', {'class': 'deal-info__wrapper'})
+        return data
+
+    @staticmethod
+    def _get_value(container: BeautifulSoup) -> str:
+        value = container.find('span', {'class': 'deal-info__value'}).text
+        return value
+
+    def get_upfront_cost(self, data: List[BeautifulSoup]) -> float:
+        upfront_cost = self._get_value(data[0])
+        upfront_cost = upfront_cost.replace('£', '')
+        return float(upfront_cost)
+
+    def get_total_cost(self, data: List[BeautifulSoup]) -> float:
+        total_cost = self._get_value(data[1])
+        total_cost = total_cost.replace('£', '')
+        return float(total_cost)
+
+    def get_allowance(self, data: List[BeautifulSoup]) -> str:
+        allowance = self._get_value(data[2])
+        return allowance
+
+    def get_contract_months(self, data: List[BeautifulSoup]) -> int:
+        contract_months = self._get_value(data[3])
+        return int(contract_months)
+
+    def get_monthly_cost(self, data: List[BeautifulSoup]) -> int:
+        monthly_cost = self._get_value(data[4])
+        monthly_cost = monthly_cost.replace('£', '')
+        return float(monthly_cost)
+
     def parse(self):
         self.soup = self._make_soup(self.URL)
+        elems = self.get_elements()
+        
--- tests/test_broadbandchoices.py
+++ tests/test_broadbandchoices.py
@@ -25,3 +25,54 @@ def test_get_provider_service(parser):
     elems = parser.get_elements()
     assert parser.get_provider_service(elems[0]) == ('Three', '4G Hub')
     assert parser.get_provider_service(elems[4]) == ('EE', '4GEE Mini Mobile WiFi')
+
+def test_get_data(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert len(data) == 5
+
+def test_get_value(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    value = parser._get_value(data[0])
+    assert value == "£0.00"
+    value = parser._get_value(data[2])
+    assert value == 'Unltd'
+
+def test_get_upfront_cost(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert parser.get_upfront_cost(data) == 0
+    data = parser._get_data(elems[1])
+    assert parser.get_upfront_cost(data) == 18
+
+
+def test_get_total_cost(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert parser.get_total_cost(data) == 408
+    data = parser._get_data(elems[1])
+    assert parser.get_total_cost(data) == 258
+
+
+def test_get_allowance(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert parser.get_allowance(data) == 'Unltd'
+    data = parser._get_data(elems[1])
+    assert parser.get_allowance(data) == 'Unltd'
+
+
+def test_get_contract_months(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert parser.get_contract_months(data) == 24
+    data = parser._get_data(elems[1])
+    assert parser.get_contract_months(data) == 12
+
+def test_get_monthly_cost(parser):
+    elems = parser.get_elements()
+    data = parser._get_data(elems[0])
+    assert parser.get_monthly_cost(data) == 17
+    data = parser._get_data(elems[1])
+    assert parser.get_monthly_cost(data) == 20
