5630aca976ef747341381bf4ecac4a7d235d5e67
Daniel Sharp <dsharpc@gmail.com>
2021-02-11 08:00:47 +0000

    started building currys desktop parser, adding tests and fixtures

--- src/parsers/currys.py
+++ src/parsers/currys.py
@@ -253,3 +253,128 @@ class CurrysProjectorParser(CurrysBaseParser):
                 )
                 projectors.append(p)
         return projectors
+
+
+class CurrysDesktopParser(CurrysBaseParser):
+    url = "https://www.currys.co.uk/gbuk/computing/desktop-pcs/desktop-pcs/317_3055_30057_xx_xx/{}_50/relevance-desc/xx-criteria.html"
+
+    def _structure_url(self, page: int = 1) -> str:
+        return self.url.format(page)
+
+    @staticmethod
+    def _parse_brand(product: BeautifulSoup) -> Tuple[str, str]:
+        brand = product.find("span", {"data-product": "brand"}).text
+        brand = brand.strip().lower()
+        return brand
+
+    @staticmethod
+    def _parse_model(product: BeautifulSoup) -> str:
+        model = product.find("span", {"data-product": "name"}).text
+        model = model.split(' - ')[0].strip().lower()
+        return model
+
+    @staticmethod
+    def _parse_price(product: BeautifulSoup) -> float:
+        price = product.find("div", {"class": "productPrices"}).text
+        price = price.strip()
+        price = price.replace(",", "")
+        price = re.search(r".(\d{1,4}[.]{0,1}\d{0,2})", price).group(1)
+        price = float(price)
+        return price
+
+    @staticmethod
+    def _parse_tech_specs(product_page: BeautifulSoup) -> Dict[str, str]:
+        table = product_page.find("div", {"class": ["tab-tech-specs"]})
+        keys = [x.text for x in table.findAll("th")]
+        values = [x.text for x in table.findAll("td")]
+        tech_specs = dict(zip(keys, values))
+        return tech_specs
+
+    @staticmethod
+    def _parse_screen_size(tech_specs: Dict[str, str]) -> Optional[float]:
+        screen = tech_specs.get("Screen size")
+        if screen is None:
+            return screen
+        size = re.search(r"(\d{2}[.]{0,1}\d{0,2})\"$", screen).group(1)
+        return float(size)
+
+    @staticmethod
+    def _parse_projection_type(tech_specs: Dict[str, str]) -> Optional[str]:
+        res = tech_specs.get("Projection type")
+        if res is None:
+            return res
+        res = res.strip().lower()
+        return res
+
+    @staticmethod
+    def _parse_resolution(tech_specs: Dict[str, str]) -> Optional[str]:
+        res = tech_specs.get("Resolution")
+        if res is None:
+            return res
+        res = res.strip().lower()
+        return res
+
+    @staticmethod
+    def _parse_brightness(tech_specs: Dict[str, str]) -> Optional[int]:
+        res = tech_specs.get("Brightness")
+        if res is None:
+            return res
+        res = re.search(r"(\d+)", res.replace(",", "")).group(1)
+        return int(res)
+
+    @staticmethod
+    def _parse_technology(tech_specs: Dict[str, str]) -> Optional[str]:
+        res = tech_specs.get("Technology")
+        if res is None:
+            return res
+        res = res.strip().lower()
+        return res
+
+    def _scrape_source(self) -> str:
+        return self.scrape_source
+
+    def _parse_scrape_url(self, product) -> str:
+        url = product.find("header", {"class": "productTitle"})
+        url = url.find("a")["href"]
+        return url
+
+    def parse(self) -> List[Projector]:
+        self.soup = self._make_soup(self._structure_url())
+        num_pages = self._get_num_pages()
+        projectors = []
+        for i in range(num_pages):
+            print(f"Downloading page: {i+1}/{num_pages}")
+            self.soup = self._make_soup(self._structure_url(i + 1))
+            products = self._get_items()
+            count = 0
+            for product in products:
+                count += 1
+                print(f"Parsing projector {count} of {len(products)}")
+                scrape_url = self._parse_scrape_url(product)
+                brand = self._parse_brand(product)
+                model = self._parse_model(product)
+                price = self._parse_price(product)
+                scrape_url = self._parse_scrape_url(product)
+                product_soup = self._make_soup(scrape_url)
+                product_tech_specs = self._parse_tech_specs(product_soup)
+                screen_size = self._parse_screen_size(product_tech_specs)
+                projection_type = self._parse_projection_type(product_tech_specs)
+                resolution = self._parse_resolution(product_tech_specs)
+                brightness = self._parse_brightness(product_tech_specs)
+                technology = self._parse_technology(product_tech_specs)
+                source = self._scrape_source()
+
+                p = Projector(
+                    brand,
+                    model,
+                    screen_size,
+                    projection_type,
+                    resolution,
+                    brightness,
+                    technology,
+                    price,
+                    source,
+                    scrape_url,
+                )
+                projectors.append(p)
+        return projectors
---
+++ tests/test_currys_desktop.py
@@ -0,0 +1,150 @@
+"""Curry's Laptop Parser Tests
+"""
+
+import pytest
+import requests
+from bs4 import BeautifulSoup
+
+from src.parsers.currys import CurrysDesktopParser
+
+
+@pytest.fixture
+def soup():
+    with open("tests/fixtures/currys/desktops_page_1.html", "r") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+
+def test_structure_url():
+    clp = CurrysDesktopParser()
+    url = clp._structure_url()
+    assert (
+        url
+        == "https://www.currys.co.uk/gbuk/computing/desktop-pcs/desktop-pcs/317_3055_30057_xx_xx/1_50/relevance-desc/xx-criteria.html"
+    )
+    url = clp._structure_url(6)
+    assert (
+        url
+        == "https://www.currys.co.uk/gbuk/computing/desktop-pcs/desktop-pcs/317_3055_30057_xx_xx/6_50/relevance-desc/xx-criteria.html"
+    )
+
+
+@pytest.fixture
+def products(soup):
+    bls = CurrysDesktopParser()
+    bls.soup = soup
+    items = bls._get_items()
+    return items
+
+
+def test_parse_brand(products):
+    clp = CurrysDesktopParser()
+    brand1 = clp._parse_brand(products[0])
+    assert brand1 == "dell"
+    brand2 = clp._parse_brand(products[4])
+    assert brand2 == "hp"
+
+
+def test_parse_model(products):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_model(products[0])
+    assert res1a == "inspiron 3881 desktop pc"
+    res2a = clp._parse_model(products[4])
+    assert res2a == "pavilion 24-k0003na 23.8\" all-in-one pc"
+
+
+def test_parse_price(products):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_price(products[0])
+    assert res1a == 399.0
+    res2a = clp._parse_price(products[4])
+    assert res2a == 779.0
+
+
+def test_parse_source():
+    clp = CurrysDesktopParser()
+    res = clp._scrape_source()
+    assert res == "currys.co.uk"
+
+
+def test_parse_scrape_url(products):
+    clp = CurrysDesktopParser()
+    res = clp._parse_scrape_url(products[0])
+    assert (
+        res
+        == "https://www.currys.co.uk/gbuk/computing/desktop-pcs/desktop-pcs/dell-inspiron-3881-desktop-pc-intel-core-i3-1-tb-hdd-black-10211691-pdt.html"
+    )
+    res = clp._parse_scrape_url(products[4])
+    assert (
+        res
+        == "https://www.currys.co.uk/gbuk/computing/desktop-pcs/desktop-pcs/hp-pavilion-24-k0003na-23-8-all-in-one-pc-amd-ryzen-5-512-gb-ssd-white-10207968-pdt.html"
+    )
+
+
+@pytest.fixture
+def single_product_screen_soup():
+    with open("tests/fixtures/currys/desktop_page_screen.html", "r") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+@pytest.fixture
+def single_product_no_screen_soup():
+    with open("tests/fixtures/currys/desktop_page_no_screen.html", "r") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+@pytest.fixture
+def tech_specs_screen():
+    data = {'Type': 'iMac', 'Operating system': 'macOS Catalina', 'Processor': '- Intel® Core™ i3 Processor\n- Quad-core\n- 3.2 GHz / 4.6 GHz', 'RAM': '8 GB DDR4 (2400 MHz)', 'Graphics card': '- AMD Radeon Pro 555X\n- 2 GB VRAM', 'Storage': '256 GB SSD', 'Touchscreen': 'No', 'Screen size': '21.5"', 'Screen type': 'Retina Display', 'Resolution': '4K Ultra HD 4096 x 2304p', 'Screen features': '- 500 nits brightness\n- Wide colour (P3)', 'WiFi': 'AC WiFi', 'Ethernet': 'Gigabit Ethernet port', 'Bluetooth': 'Bluetooth 4.2', 'USB': 'USB 3.0 x 4', 'Video connections': 'Thunderbolt 3 x 2', 'Audio connections': '3.5 mm jack', 'Speakers': 'Yes', 'Disc drive': 'No', 'Memory card reader': 'SDXC card reader', 'Camera': 'FaceTime HD camera', 'Mouse / trackpad': 'Apple Magic Mouse 2', 'Keyboard': 'Apple Magic Keyboard', 'Colour': 'Aluminium', 'Box contents': '- Apple iMac 4K 21.5" (2020)\n- Magic keyboard\n- Magic mouse 2\n- Power cable\n- Lightning to USB cable', 'Dimensions': '450 x 528 x 175 mm (H x W x D)', 'Weight': '5.60 kg', 'Manufacturer’s guarantee': '1 year', 'Software': '- Photos\n- iMovie\n- GarageBand\n- Pages\n- Numbers\n- Keynote\n- Siri\n- Safari\n- Mail\n- FaceTime\n- Messages\n- Maps\n- News\n- Stocks\n- Home\n- Voice Memos\n- Notes\n- Calendar\n- Contacts\n- Reminders\n- Photo Booth\n- Preview\n- iTunes\n- Books\n- App Store\n- Time Machine'}
+    return data
+
+@pytest.fixture
+def tech_specs_no_screen():
+    data = {'Type': 'Desktop PC', 'Operating system': 'Windows 10', 'Processor': '- Intel® Core™ i3-10100 Processor\n- Quad-core\n- 3.6 GHz / 4.3 GHz\n- 6 MB cache', 'RAM': '- 8 GB DDR4 (2666 MHz)\n- 64 GB maximum installable RAM', 'Storage': '1 TB HDD (7200 rpm)', 'WiFi': '- Dual-band AC WiFi\n- 1x1\n- Up to 433 Mbps', 'Ethernet': 'Gigabit Ethernet port', 'Bluetooth': 'Bluetooth 5.0', 'USB': '- USB Type-C x 1\n- USB 2.0 x 4', 'Video connections': '- HDMI 1.4 x 1\n- DisplayPort x 1\n- VGA x 1', 'Audio connections': '3.5 mm jack x 2', 'Audio software': 'Waves MaxxAudio Pro', 'Disc drive': 'DVD', 'Memory card reader': 'SD', 'Mouse / trackpad': 'USB mouse', 'Keyboard': 'USB keyboard with numeric keypad', 'Security features': '- Padlock ring security lock slot\n- Security cable wedge-shaped lock slot', 'Colour': 'Black', 'Box contents': '- Dell Inspiron 3881 Desktop PC (348P9)\n- Power adapter\n- Documentation', 'Dimensions': '324.3 x 154 x 293 mm (H x W x D)', 'Weight': '6.4 kg', 'Manufacturer’s guarantee': '1 year', 'Software': '* Full version of Microsoft Office not included\n* Full version of anti-virus / internet security not included\n\n- Office 2013 (30 day trial)\n- McAfee (30 day trial)'}
+    return data
+
+def test_parse_tech_specs_screen(single_product_screen_soup, tech_specs_screen):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_tech_specs(single_product_screen_soup)
+    assert res1a == tech_specs_screen
+
+def test_parse_tech_specs(single_product_no_screen_soup, tech_specs_no_screen):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_tech_specs(single_product_no_screen_soup)
+    assert res1a == tech_specs_no_screen
+
+def test_parse_screen_size(tech_specs_screen):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_screen_size(tech_specs_screen)
+    assert res1a == 21.5
+
+def test_parse_screen_size_no_screen(tech_specs_no_screen):
+    clp = CurrysDesktopParser()
+    res1a = clp._parse_screen_size(tech_specs_no_screen)
+    assert res1a == None
+
+# def test_parse_projection_type(tech_specs):
+#     clp = CurrysDesktopParser()
+#     res1a = clp._parse_projection_type(tech_specs)
+#     assert res1a == "short throw"
+
+
+# def test_parse_resolution(tech_specs):
+#     clp = CurrysDesktopParser()
+#     res1a = clp._parse_resolution(tech_specs)
+#     assert res1a == "1920 x 1080"
+
+
+# def test_parse_brightness(tech_specs):
+#     clp = CurrysDesktopParser()
+#     res1a = clp._parse_brightness(tech_specs)
+#     assert res1a == 3000
+
+
+# def test_parse_technology(tech_specs):
+#     clp = CurrysDesktopParser()
+#     res1a = clp._parse_technology(tech_specs)
+#     assert res1a == "dlp"
