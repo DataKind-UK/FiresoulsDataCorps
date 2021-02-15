259ae4d900f0573d20b267a4a5533d631322281e
Diego Arenas <darenasc@gmail.com>
2021-02-15 21:06:16 +0000

    desktop with incremental update

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import pymysql.cursors
 import pandas as pd
 import math
+import datetime
 
 
 def get_db_connection():
@@ -19,51 +20,93 @@ def get_sql_script(table: str):
 
 
 def insert_into_desktop(df: pd.DataFrame):
+    """
+    Insert new data in the `desktop` table.
+    Parameters:
+        df (pd.DataFrame):
+    """
+
+    def check_if_desktop_exists(url):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        valid_from = datetime.datetime.now()
+        sql = """select scrape_url, version
+                from desktop
+                where scrape_url = '{}'
+                    and valid_to is null;""".format(
+            url
+        )
+        cursor.execute(sql)
+        rows = cursor.fetchone()
+        cursor.close()
+
+        if rows is None:
+            version = 1
+        else:
+            version = rows["version"]
+            cursor2 = connection.cursor()
+            update_query = """update desktop set valid_to = '{}' 
+                                where scrape_url = '{}'
+                                and version = {};""".format(
+                valid_from, url, version
+            )
+            cursor2.execute(update_query)
+            connection.commit()
+            version += 1
+            cursor2.close()
+        connection.close()
+        return version, valid_from
+
     connection = get_db_connection()
+    cursor = connection.cursor()
+
     df1 = df.where(pd.notnull(df), None)
-    with connection:
-        with connection.cursor() as cursor:
-            sql = """insert into desktop (
-            brand, 
-            model, 
-            processor, 
-            screen_size,
-            ram, 
-            storage_hdd, 
-            storage_ssd,
-            release_year, 
-            optical_drive,
-            operative_system,
-            price, 
-            scrape_source, 
-            scrape_url, 
-            scrape_date) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
-            for i, r in tqdm(df1.iterrows(), total=len(df), desc="desktops"):
-                # print(r)
-                # for c in df.columns:
-                #    if isinstance(r[c], float):
-                #        if math.isnan(r[c]):
-                #            r[c] = None
-                cursor.execute(
-                    sql,
-                    (
-                        r["brand"],
-                        r["model"],
-                        r["processor"],
-                        str(r["screen_size"]),
-                        r["ram"],
-                        r["storage_hdd"],
-                        r["storage_ssd"],
-                        str(r["release_year"]),
-                        r["optical_drive"],
-                        r["operative_system"],
-                        r["price"],
-                        r["scrape_source"],
-                        r["scrape_url"],
-                        r["scrape_date"],
-                    ),
-                )
+    df1.drop_duplicates(inplace=True)
+
+    sql = """insert into desktop (
+        brand, 
+        model, 
+        processor, 
+        screen_size,
+        ram, 
+        storage_hdd, 
+        storage_ssd,
+        release_year, 
+        optical_drive,
+        operative_system,
+        price, 
+        scrape_source, 
+        scrape_url, 
+        scrape_date,
+        valid_from,
+        version) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+    for i, r in tqdm(df1.iterrows(), total=len(df1), desc="desktops"):
+        version, valid_from = check_if_desktop_exists(r["scrape_url"])
+        cursor.execute(
+            sql,
+            (
+                r["brand"],
+                r["model"],
+                r["processor"],
+                str(r["screen_size"]),
+                r["ram"],
+                r["storage_hdd"],
+                r["storage_ssd"],
+                str(r["release_year"]),
+                r["optical_drive"],
+                r["operative_system"],
+                r["price"],
+                r["scrape_source"],
+                r["scrape_url"],
+                r["scrape_date"],
+                valid_from,
+                version,
+            ),
+        )
         connection.commit()
+    cursor.close()
+    connection.close()
 
 
 def insert_into_laptop(df: pd.DataFrame):
